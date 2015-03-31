8bcb944a6d86e734a3d953d05eeaf8f977285674
Iliana Bobeva <iliana.bobeva@gmail.com>
2015-04-01 01:44:12 +0300

    Comment class added

---
+++ Library/Library/Comment.cs
@@ -0,0 +1,18 @@
+﻿namespace Library
+{
+    using System;
+
+    public class Comment
+    {
+        private string Author { get; private set; }
+        private DateTime PublishingDate { get; private set; }
+        private string Text { get; private set; }
+
+        public Comment(string author, string text)
+        {
+            this.Author = author;
+            this.Text = text;
+            this.PublishingDate = DateTime.Now;
+        }
+    }
+}
