b17744248295775609b5f0de267d0963bd810d0e
Dani-Ivanov <dani.v.ivanov@gmail.com>
2015-04-02 13:58:51 +0300

    Added Login.cs , FileManager.cs , UserPanel.xaml. Modified MainWindow and added reference to Library. Added some folders :D

---
+++ Library/Library/Login.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    static class Login
+    {
+        static string message = "Wrong username or password.";
+        static string username;
+        static string password;
+        static string filePath;
+        static string Password
+        {
+            set
+            {
+                if (value != FileManager.GetUserPassword(username))
+                {
+                    message = "Wrong password!";
+                    //throw new Exception(message);
+                    throw new ArgumentException(message);
+                }
+            }
+        }
+
+        static string Username
+        {
+            set
+            {
+                username = value;
+            }
+        }
+
+        public static void Init(string username, string password)
+        {
+
+            if (!FileManager.UserExist(username))
+            {
+                message = "Username does not exist!";
+                throw new ArgumentException(message);
+            }
+            Username = username;
+            Password = password;
+        }
+    }
+}
