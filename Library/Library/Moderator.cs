b7db0eb76248de38030b434d027e13cfb009ed38
Petar Zubev <p-zubev@operamail.com>
2015-04-02 11:49:11 +0300

    Class RegularUser and Moderator Added

---
+++ Library/Library/Moderator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class Moderator : User, IModerator
+    {
+        public void RemoveComment()
+        {
+            throw new NotImplementedException();
+        }
+        
+        public string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Password
+        {
+            set { throw new NotImplementedException(); }
+        }
+    }
+}
---
+++ Library/Library/RegularUser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class RegularUser : User, IRegularUser
+    {
+        
+        public string Name
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string Password
+        {
+            set { throw new NotImplementedException(); }
+        }
+
+        
+    }
+}
